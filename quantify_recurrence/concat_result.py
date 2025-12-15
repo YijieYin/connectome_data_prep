9b5057fa74d85827aecabf1c223a7055b10c2a5f
YijieYin <yy432@cam.ac.uk>
2025-12-15 00:02:08 +0000

    update to concat single cell level

--- quantify_recurrence/concat_result.py
+++ quantify_recurrence/concat_result.py
@@ -1,8 +1,8 @@
 import glob, pandas as pd
 import os
 
-files = glob.glob("recurrence_results_rank*.csv")
+files = glob.glob("sc_recurrence_results_rank*.csv")
 df = pd.concat(pd.read_csv(f) for f in files)
-df.to_csv("recurrence_results.csv", index=False)
+df.to_csv("sc_recurrence_results.csv", index=False)
 for f in files:
     os.remove(f)
