47561e0cd5c388480d42210a992a388262aa8857
YijieYin <yy432@cam.ac.uk>
2026-03-16 12:17:56 +0000

    FAFB axon-dendrite split

--- fafb_ad_split/ad_split.sh
+++ fafb_ad_split/ad_split.sh
@@ -7,19 +7,19 @@
 #SBATCH --output=result_%A.out  # %A is replaced by job ID, %a by array index
 #SBATCH --error=error_%A.err
 
-# Activate your Python environment - you should be in folder dev 
+# Activate your Python environment - you should be in folder interpret_connectome 
 source ../.bashrc
 
 conda activate flyconnectome
 
 # first general axon-dendrite split, to calculate e.g. segregation index
 # writing to axon_in, axon_out, dendrite_in, dendrite_out folders
-time srun python Python/fafb_ad_split/axon_dendrite_split.py
+time srun python fafb_ad_split/axon_dendrite_split.py
 # manual fixes, has the same content as axon_dendrite_split_FAFB.ipynb
 # writing to axon_in, axon_out, dendrite_in, dendrite_out folders
-python Python/fafb_ad_split/manual_split.py
+python fafb_ad_split/manual_split.py
 # put together into edge list for each neuron in folder syn_count
-time srun python Python/fafb_ad_split/make_el.py
+time srun python fafb_ad_split/make_el.py
 # then go to axon_dendrite_split_FAFB.ipynb, section make_adj, to get the overall results 
 
 # to re-run, first remove all contents of : 
